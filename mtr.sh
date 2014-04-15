7dd815348aa927f320c2c0f28895787f5758b156
froyomuffin <froyomuffin@gmail.com>
2014-04-14 22:07:44 -0400

    GA: Added URL decoding & cp

--- mtr.sh
+++ mtr.sh
@@ -1,23 +1,40 @@
 #!/bin/bash
 
+DEST="~/Desktop/land/"
+BGCPBUFFER="10"
+
 function getTrackIdList {
 	PLAYLISTNAME=$1
 	TRACKLISTSEDSTRING="/<key>Name<\/key><string>$PLAYLISTNAME/,/<\/array>/p"
 	cat lib.xml | sed -n $TRACKLISTSEDSTRING | grep '<key>Track ID</key>' | awk -F">|<" '{print $7}' | sort -n | uniq
 }
 
-function getTrackLocation {
-	TRACKID=$1
-	echo "meep"
-	echo $1
-	TRACKIDSEDSTRING="/<\/key><integer>$TRACKID<\/integer>/,/<\/dict>/p"
+function urldecode {
+    # Credits to Chris Down
+    # https://gist.github.com/cdown
 
-	cat lib.xml | sed -n $TRACKIDSEDSTRING | grep '<key>Location</key><string>' | awk -F">|<" '{print $7}' | sed 's|file:\/\/localhost||g' | sed 's/%20/\\ /g'
+    local url_encoded="${1//+/ }"
+    printf '%b' "${url_encoded//%/\x}"
+    echo ""
 }
 
-TRACKLIST=`getTrackIdList "Send"`
-echo "moo"
-SINGLE=`echo $TRACKLIST | awk '{ print $3 }'`
-echo $SINGLE
+function getTrackLocation {
+	TRACKID=$1
+	TRACKIDSEDSTRING="/<\/key><integer>$TRACKID<\/integer>/,/<\/dict>/p"
 
-getTrackLocation $SINGLE
+	urldecode `cat lib.xml | sed -n $TRACKIDSEDSTRING | grep '<key>Location</key><string>' | awk -F">|<" '{print $7}' | sed 's|file:\/\/localhost||g' | sed 's/\&#38;/\&/g'`
+}
+
+TRACKLIST=`getTrackIdList "Subset"`
+SINGLE=`echo $TRACKLIST | awk '{ print $3 }'`
+
+#TRACKARR=()
+
+for SINGLE in $TRACKLIST
+do
+	#TRACKARR+=`getTrackLocation $SINGLE`
+	FILE=`getTrackLocation $SINGLE`
+	cp "$FILE" $DEST
+done
+
+echo "$TRACKARR"
